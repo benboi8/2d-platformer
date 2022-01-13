d86e7b59e024100ee83d82d761e93e377d9ee9b2
ben williams <45974327+benboi8@users.noreply.github.com>
2022-01-13 22:27:56 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # end point
-# coins / scoring system
 # leader board
 # enemies - event system / left to right collisions for ai
-# timer
 # level editor
+# goo / water / danger system
+# sprinting physics
+
+# AI
+# Multiple states:
+# - attack close
+# - attack far
+# - patrol
+# - idle
+
+# - move side between barriers
+# - up and down
+# - ranged attacks
+# - http://hyperphysics.phy-astr.gsu.edu/hbase/traj.html
+
+# - restarting
+# - pause menu
+
 
 import os
 import sys
@@ -11,19 +27,41 @@ import sys
 os.chdir(sys.path[0])
 sys.path.insert(1, "P://Python Projects/assets/")
 
+
 from GameObjects import *
+from GUIShapes import *
 
 allImages = []
 allPlaforms = []
 allLadders = []
+allStairs = []
+allCoins = []
 
 allObjs = []
 
-class Image(Box):
+
+class BoxObj(Box):
+	def __init__(self, rect, colors, name="", surface=screen, drawData={}, lists=[allObjs]):
+		super().__init__(rect, colors, name, surface, drawData, lists)
+
+	def Move(self, direction):
+		if self.name not in ["roof", "leftWall", "rightWall", "floor"]:
+			if direction[0]:
+				if not player.colliding["right"]:
+					self.rect.x -= player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
+			elif direction[1]:
+				if not player.colliding["left"]:
+					self.rect.x += player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
+
+
+
+class Image(BoxObj):
 	def __init__(self, rect, imagePath, name="", surface=screen, lists=[allImages, allObjs]):
 		super().__init__(rect, ((0, 0, 0), (0, 0, 0)), name, surface, drawData={"drawBackground": False}, lists=lists)
 
 		self.imagePath = imagePath
+
+		self.resize = False
 
 		self.ScaleImage(pg.image.load(self.imagePath) if self.imagePath != None else None, (self.rect.w, self.rect.h))
 
@@ -89,20 +127,6 @@ class Collider:
 		return self.rect.colliderect(rect)
 
 
-class BoxObj(Box):
-	def __init__(self, rect, colors, name="", surface=screen, drawData={}, lists=[allObjs]):
-		super().__init__(rect, colors, name, surface, drawData, lists)
-
-	def Move(self, direction):
-		if self.name not in ["roof", "leftWall", "rightWall", "floor"]:
-			if direction[0]:
-				if not player.colliding["right"]:
-					self.rect.x -= player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
-			elif direction[1]:
-				if not player.colliding["left"]:
-					self.rect.x += player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
-
-
 class Platform(BoxObj):
 	def __init__(self, rect, colors, imagePath=None, name="", surface=screen, drawData={}, lists=[allPlaforms, allObjs]):
 		super().__init__(rect, colors, name, surface, drawData, lists)
@@ -117,13 +141,44 @@ class Platform(BoxObj):
 			self.image.Draw()
 
 
+class Stairs:
+	def __init__(self, rect, colors, stepRect, imagePath=None, name="", surface=screen, drawData={}, lists=[allObjs, allStairs]):
+		self.rect = pg.Rect(rect)
+
+		self.backgroundColor = colors[0]
+		self.borderColor = colors[1]
+
+		self.stepRect = stepRect
+
+		AddToListOrDict(lists, self)
+
+		self.steps = [Platform((self.rect.x + (i * self.stepRect[0]), (self.rect.y - (i * self.stepRect[2] * self.stepRect[3])) + (self.rect.h if self.stepRect[3] == 1 else 0), self.stepRect[0], self.stepRect[1]), colors, imagePath, drawData={"borderWidth": 1}) for i in range(self.rect.w // self.stepRect[0])]
+
+	def Move(self, direction):
+		if direction[0]:
+			if not player.colliding["right"]:
+				self.rect.x -= player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
+		elif direction[1]:
+			if not player.colliding["left"]:
+				self.rect.x += player.movementSpeed * (player.sprintSpeedMulti if player.isSprinting else 1) 
+
+	def Draw(self):
+		pg.draw.rect(screen, red, self.rect)
+		if self.stepRect[3] == -1:
+			pg.draw.aaline(screen, black, (self.rect.x, self.rect.y), (self.rect.x + self.rect.w, self.rect.y + self.rect.h))
+		if self.stepRect[3] == 1:
+			pg.draw.aaline(screen, black, (self.rect.x, self.rect.y + self.rect.h), (self.rect.x + self.rect.w, self.rect.y))
+
+
 class Ladder(BoxObj):
 	def __init__(self, rect, colors, imagePath=None, name="", surface=screen, drawData={}, lists=[allLadders, allObjs]):
 		super().__init__(rect, colors, name, surface, drawData, lists)
 
 		self.image = Image(self.rect, imagePath, lists=[]) if imagePath != None else None
 
-		self.rungs = [pg.Rect(self.rect.x, self.rect.y + (i + 1) * self.rect.h // 10 - 2, self.rect.w, 4) for i in range(self.rect.h // 20)]
+		numOfRungs = self.rect.h // 20
+		spacing = self.rect.h // numOfRungs
+		self.rungs = [pg.Rect(self.rect.x, self.rect.y + (i * spacing), self.rect.w, 4) for i in range(numOfRungs)]
 
 	def Draw(self):
 		self.DrawBackground()
@@ -165,9 +220,14 @@ class Player(Box):
 		self.crouchData = {"height": self.rect.h // 1.5, "moveSpeed": 3}
 		self.height = self.rect.h
 		self.ladder = None
-		self.climbSpeed = 6`
+		self.climbSpeed = 6
+		self.climbFallSpeed = 1
 		self.climbDirection = -1
 		self.j = 0
+
+		self.coins = 0
+
+		self.coinCounter = Label((self.rect.x + self.rect.w // 2 - 15, self.rect.y - 30, 30, 20), colors, "0", textData={"fontSize": 15}, drawData={"drawBackground": True, "drawBorder": False})
 
 		self.direction = [False, False]
 
@@ -177,6 +237,8 @@ class Player(Box):
 
 		if self.image != None:
 			self.image.Draw()
+
+		self.coinCounter.UpdateRect((self.rect.x + self.rect.w // 2 - 15, self.rect.y - 30, 30, 20))
 
 	def HandleEvent(self, event):
 		if event.type == pg.KEYDOWN:
@@ -239,10 +301,8 @@ class Player(Box):
 			if self.grounded:
 				self.isJumping = True
 
-		if self.climbing:
-			self.Climb()
-		else:
-			self.Crouch()
+		self.Climb()
+		self.Crouch()
 
 		if not self.isJumping:
 			if self.ladder == None:
@@ -250,36 +310,46 @@ class Player(Box):
 				# [left, right], [up, down] 
 				self.ApplyForce(self.gravity, [[False, False], [False, True]])
 
+		if self.rect.x >= ep.rect.x:
+			self.won = True
+			ts.Stop()
+
 	def Climb(self):
-		canClimb = False
 		if self.ladder != None:
-			if self.climbDirection == -1:
-				if self.rect.y + self.rect.h > self.ladder.rect.y:
-					canClimb = True
-			
-			if self.climbDirection == 1:
-				if self.rect.y + self.rect.h < self.ladder.rect.y + self.ladder.rect.h:
-					canClimb = True
+			if self.climbing:
+				canClimb = False
+				if self.climbDirection == -1:
+					if self.rect.y + self.rect.h > self.ladder.rect.y:
+						canClimb = True
+				
+				if self.climbDirection == 1:
+					if self.rect.y + self.rect.h < self.ladder.rect.y + self.ladder.rect.h:
+						canClimb = True
 
+				if canClimb:
+					# self.rect.y += self.climbSpeed * self.climbDirection
+					self.ApplyForce(self.climbSpeed, [[False, False], [True if self.climbDirection == -1 else False, True if self.climbDirection == 1 else False]])
 
-		if canClimb:
-			self.rect.y += self.climbSpeed * self.climbDirection
+			else:
+				# self.rect.y += self.climbFallSpeed
+				self.ApplyForce(self.climbFallSpeed, [[False, False], [False, True]])
 
 	def Crouch(self):
-		if self.crouched:
-			self.rect.h = self.crouchData["height"]
-			self.movementSpeed = self.crouchData["moveSpeed"]
-		else:
-			if self.rect.h != self.height:
-				for i in range(3):
-					if not self.colliding["up"]:
-						self.rect.h = self.height
-						self.movementSpeed = self.moveSpeed
-					else:
-						self.rect.h = self.crouchData["height"]
-						self.movementSpeed = self.crouchData["moveSpeed"]
+		if not self.climbing:
+			if self.crouched:
+				self.rect.h = self.crouchData["height"]
+				self.movementSpeed = self.crouchData["moveSpeed"]
+			else:
+				if self.rect.h != self.height:
+					for i in range(3):
+						if not self.colliding["up"]:
+							self.rect.h = self.height
+							self.movementSpeed = self.moveSpeed
+						else:
+							self.rect.h = self.crouchData["height"]
+							self.movementSpeed = self.crouchData["moveSpeed"]
 
-					self.Collide()
+						self.Collide()
 
 	def Jump(self):
 		if self.isJumping:
@@ -353,9 +423,107 @@ class Player(Box):
 					self.ladder = ladder
 					break
 
+		# stairs
+		self.stairs = None
+		for stairs in allStairs:
+			for collider in self.colliders:
+				if collider.CollideCheck(stairs.rect):
+					self.stairs = stairs
+					break
+
+		if self.stairs != None:
+			self.colliding["left"] = False
+			self.colliding["right"] = False
+
+
+		# coins
+		for coin in allCoins:
+			for collider in self.colliders:
+				if collider.CollideCheck(coin.rect):
+					self.coins += coin.Collect()
+					self.coinCounter.UpdateText(str(self.coins))
+
+
+class Coin(BoxObj):
+	def __init__(self, rect, colors, value=1, pickUpSound=None, imagePath=None, name="", surface=screen, drawData={}, lists=[allObjs, allCoins]):
+		super().__init__(rect, colors, name, surface, drawData, lists)
+
+		self.pickUpSound = pickUpSound
+
+		self.value = value
+
+		self.image = Image(self.rect, imagePath, lists=[]) if imagePath != None else None
+
+	def Draw(self):
+		self.DrawBackground()
+		self.DrawBorder()
+
+	def Collect(self):
+		if self in allObjs:
+			allObjs.remove(self)
+		if self in allCoins:
+			allCoins.remove(self)
+			return self.value
+		return 0
+
+
+class TimeScore:
+	def __init__(self):
+		self.startTime = dt.datetime.now()
+
+		self.hours = 0
+		self.minutes = 0
+
+		self.timer = Label((width - 210, 10, 200, 50), (lightBlack, darkWhite), drawData={"roundedCorners": True, "roundness": 2}, textData={"fontSize": 30})
+
+		self.paused = False
+		self.stopped = False
+
+	def Reset(self):
+		self.startTime = dt.datetime.now()
+
+	def Pause(self):
+		self.paused = not self.paused
+
+	def Stop(self):
+		self.stopped = True
+
+	def Update(self):
+		if not self.paused and not self.stopped:
+			self.difference = dt.datetime.now() - self.startTime
+
+			if self.difference.seconds % 60 == 0:
+				self.minutes = self.difference.seconds // 60
+
+			if self.difference.seconds % 3600 == 0:
+				self.hours = self.difference.seconds // 3600
+
+			msg = f"{self.hours}:{self.minutes}:{self.difference.seconds}:{str(self.difference.microseconds)[0]}"
+			self.timer.UpdateText(msg)
+			return msg
+
+
+class EndPoint(BoxObj):
+	def __init__(self, rect, colors, imagePath=None):
+		super().__init__(rect, colors)
+		# self.image = Image(rect, imagePath)
+
+
+	def Draw(self):
+		pg.draw.rect(screen, self.backgroundColor, self.rect)
+		pg.draw.rect(screen, self.backgroundColor, (self.rect.x - 5, self.rect.y, 5, 100))
+		DrawRectOutline(self.borderColor, self.rect)
+		DrawRectOutline(self.borderColor, pg.Rect(self.rect.x - 5, self.rect.y, 5, 100))
+
 
 def DrawLoop():
 	screen.fill(darkGray)
+
+	# for stairs in allStairs:
+	# 	if stairs.rect.colliderect(cam.rect):
+	# 		stairs.Draw()
+	
+	ep.Draw()
 
 	for image in allImages:
 		if image.rect.colliderect(cam.rect):
@@ -368,9 +536,14 @@ def DrawLoop():
 	for ladder in allLadders:
 		if ladder.rect.colliderect(cam.rect):
 			ladder.Draw()
+
+	for coin in allCoins:
+		if coin.rect.colliderect(cam.rect):
+			coin.Draw()
 	
 	player.Draw()
 
+	
 	cam.Draw()
 
 	# pg.draw.rect(screen, red, centerMarker.rect)
@@ -391,6 +564,8 @@ def Update():
 
 	cam.Update(player)
 
+	ts.Update()
+
 
 
 # background = Image((0, 0, width, height), "background.jpg")
@@ -400,28 +575,50 @@ cam = Camera((0, 0, width, height), None, darkWhite)
 
 fpsLbl = Label((0, 0, 100, 50), (lightBlack, darkWhite), str(fps), textData={"fontSize": 12, "alignText": "left-top"}, drawData={"drawBackground": False, "drawBorder": False})
 
-
-floor = Platform((0, height // 2 + 350, width, 100), (lightBlack, darkWhite), name="floor")
-Platform((0, -2, width, 2), (lightBlack, darkWhite), name="roof")
-Platform((-2, 0, 2, height), (lightBlack, darkWhite), name="leftWall")
-Platform((width, 0, 2, height), (lightBlack, darkWhite), name="rightWall")
+floor = Platform((0, height - 20, width, 20), (lightBlack, darkWhite), name="floor")
+Platform((0, -12, width, 12), (lightBlack, darkWhite), name="roof")
+Platform((-12, 0, 12, height), (lightBlack, darkWhite), name="leftWall")
+Platform((width, 0, 12, height), (lightBlack, darkWhite), name="rightWall")
 centerMarker = GameObject((width // 2, 0, 10, 10))
 
+y = height - floor.rect.h
 
-Platform((200, height // 2 + 300, 100, 50), (lightBlack, darkWhite))
-Platform((700, height // 2 + 300 - 20, 200, 70), (lightBlack, darkWhite))
-Platform((800, height // 2 + 300 - 70, 100, 50), (lightBlack, darkWhite))
-Platform((300, 290 + 300, 200, 20), (lightBlack, darkWhite))
-Platform((500, 230 + 300, 150, 20), (lightBlack, darkWhite))
-Platform((650, 190 + 300, 150, 20), (lightBlack, darkWhite))
-Platform((1100, height // 2 - 150 + 300, 50, 200), (lightBlack, darkWhite))
-Platform((900, height // 2 - 10 + 300, 50, 60), (lightBlack, darkWhite))
-Platform((1050, height // 2 - 50 + 300, 50, 100), (lightBlack, darkWhite))
-Ladder((1150, height // 2 - 150 + 100, 50, 400), (lightBlack, darkWhite))
+Platform((200, y - 50, 100, 50), (lightBlack, darkWhite))
+Platform((700, y - 70, 200, 70), (lightBlack, darkWhite))
+Platform((800, y - 120, 100, 50), (lightBlack, darkWhite))
+Platform((300, y - 120, 200, 20), (lightBlack, darkWhite))
+Platform((510, y - 180, 150, 20), (lightBlack, darkWhite))
+Platform((690, y - 260, 150, 20), (lightBlack, darkWhite))
+Platform((1100, y - 200, 50, 200), (lightBlack, darkWhite))
+Platform((900, y - 60, 50, 60), (lightBlack, darkWhite))
+Platform((1050, y - 100, 50, 100), (lightBlack, darkWhite))
+Ladder((1150, y - 500, 50, 500), (lightBlack, darkWhite))
+Platform((1200, y - 500, 20, 465), (lightBlack, darkWhite))
 
-player = Player((10, height // 2 + 360, 20, 50), (lightBlack, darkWhite))
+Stairs((1300, y - 110, 360, 110), (lightBlack, darkWhite), (10, 20, 3, 1))
+Platform((1660, y - 110, 20, 110), (lightBlack, darkWhite))
 
-fps = 60
+Platform((1200, y - 500, 100, 20), (lightBlack, darkWhite))
+Stairs((1300, y - 590, 300, 90), (lightBlack, darkWhite), (10, 20, 3, 1))
+Platform((1600, y - 590, 100, 20), (lightBlack, darkWhite))
+Stairs((1700, y - 590, 600, 180), (lightBlack, darkWhite), (10, 20, 3, -1))
+Platform((2300, y - 410, 300, 20), (lightBlack, darkWhite))
+Platform((2600, 0, 20, 310), (lightBlack, darkWhite))
+
+Coin((235, y - 90, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((385, y - 160, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((565, y - 220, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((755, y - 300, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((755, y - 110, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((1060, y - 145, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+Coin((1640, y - 630, 30, 35), (ChangeColorBrightness(yellow, 50), yellow), drawData={"borderWidth": 3})
+
+ep = EndPoint((2450, y - 510, 30, 30), (lightBlack, darkWhite), "flag.png")
+
+player = Player((10, y - 55, 20, 50), (lightBlack, darkWhite))
+
+ts = TimeScore()
+
 while running:
 	clock.tick_busy_loop(fps)
 	deltaTime = clock.get_time()
